ac19f2a09b6671254e569ff48175a4e73f8c537b
kgho <kuanggh@outlook.com>
2020-08-25 09:26:20 +0800

    接收用户消息

---
+++ Test/src/Servlet/WxServlet.java
@@ -0,0 +1,77 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.WxService;
+
+/**
+ * Servlet implementation class WxServlet
+ */
+@WebServlet("/wx")
+public class WxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public WxServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("get");
+		String signature = request.getParameter("signature");
+		String timestamp = request.getParameter("timestamp");
+		String nonce = request.getParameter("nonce");
+		String echostr = request.getParameter("echostr");
+		System.out.println(signature);
+		System.out.println(timestamp);
+		System.out.println(nonce);
+		System.out.println(echostr);
+
+		// 校验证请求
+		if (WxService.check(timestamp, nonce, signature)) {
+			PrintWriter out = response.getWriter();
+			// 原样返回echostr参数
+			out.print(echostr);
+			out.flush();
+			out.close();
+			System.out.println("接入成功");
+		} else {
+			System.out.println("接入失败");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// doGet(request, response);
+		System.out.println("post");
+
+		// 处理消息和事件的推送
+		Map<String,String> requestMap = WxService.parseRequest(request.getInputStream());
+		System.out.println(requestMap);
+
+	}
+
+}
---
+++ Test/src/service/WxService.java
@@ -0,0 +1,89 @@
+package service;
+
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+public class WxService {
+	private static final String TOKEN = "test";
+
+	/**
+	 * 验证签名
+	 * 
+	 * @param timestamp
+	 * @param nonce
+	 * @param signature
+	 * @return
+	 */
+	public static boolean check(String timestamp, String nonce, String signature) {
+
+		// TODO Auto-generated method stub1）将token、timestamp、nonce三个参数进行字典序排序
+		String[] strs = new String[] { TOKEN, timestamp, nonce };
+		Arrays.sort(strs);
+
+		// 2）将三个参数字符串拼接成一个字符串进行sha1加密
+		String str = strs[0] + strs[1] + strs[2];
+		String mysig = sha1(str);
+		System.out.println(mysig);
+		System.out.println(signature);
+
+		// 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信urn false;
+
+		return mysig.equalsIgnoreCase(signature);
+	}
+
+	private static String sha1(String src) {
+
+		try {
+			// 获取一个加密对象
+			MessageDigest md = MessageDigest.getInstance("sha1");
+			// 加密
+			byte[] digest = md.digest(src.getBytes());
+			char[] chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+			StringBuilder sb = new StringBuilder();
+			// 处理加密结果
+			for (byte b : digest) {
+				sb.append(chars[(b >> 4) & 15]);
+				sb.append(chars[b & 15]);
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * 解析xml数据包
+	 * 
+	 * @param is
+	 * @return
+	 */
+	public static Map<String, String> parseRequest(InputStream is) {
+		Map<String,String> map = new HashMap<>();
+		SAXReader reader = new SAXReader();
+		try {
+			//读取输入流，获取文档对象
+			Document document = reader.read(is);
+			//根据文档对象获取根节点
+			org.dom4j.Element root =  document.getRootElement();
+			//获取根节点的所有子节点
+			List<org.dom4j.Element> elements =   root.elements();
+			for(org.dom4j.Element e:elements) {
+				map.put(e.getName(), e.getStringValue());
+			}
+		}catch(DocumentException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+}
