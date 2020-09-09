8bad78f224b6ed79f57234fe4fd5f17b5c2701d1
kgho <kuanggh@outlook.com>
2020-09-09 16:18:13 +0800

    拉取用户基本信息

--- Test/src/servlet/GetUserInfo.java
+++ Test/src/servlet/GetUserInfo.java
@@ -7,33 +7,56 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONObject;
+import util.Util;
+
 /**
  * Servlet implementation class GetUserInfo
  */
 @WebServlet("/GetUserInfo")
 public class GetUserInfo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetUserInfo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public GetUserInfo() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("GetUserInfo");
+
+		// 2.��ȡcode
+		String code = request.getParameter("code");
+		// 3.��ȡaccesstoken�ĵ�ַ
+		String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+		url = url.replace("APPID", "wx6f2f69bf193c2c3f").replace("SECRET", "3470f23a5f8d75c24762793851e596ee")
+				.replace("CODE", code);
+		String result = Util.get(url);
+		String at = JSONObject.fromObject(result).getString("access_token");
+		String openid = JSONObject.fromObject(result).getString("openid");
+		// 4.��ȥ�û��Ļ�����Ϣ
+		url = "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
+		url = url.replace("ACCESS_TOKEN", at).replace("OPENID", openid);
+
+		result = Util.get(url);
+		System.out.println(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
